71da91d591bf44da9d74926e5639147f08efdffa
TianzeWang609 <tw609@cam.ac.uk>
2026-05-17 06:54:08 +0100

    Complete part 1 FK and custom motion

--- viewer/student_submission/part1_fk.py
+++ viewer/student_submission/part1_fk.py
@@ -27,4 +27,40 @@ def forward_kinematics(
         (joint_count, 1, 1),
     )
     world_positions = np.zeros((joint_count, 3), dtype=np.float32)
+    if topological_order is not None: # this is to decide the processing order, make sure parent first and then child
+        order = list(topological_order)
+    else:
+        
+        children = [[] for _ in range(joint_count)] # code to test: order = list(range(joint_count))
+        # children[i] is to store all the children of joint i, roots is to store all the root joints with parent=-1
+        roots = []         
+        for i, joint in enumerate(joints):
+            parent = joint.parent
+            if parent is None or parent <0:
+                roots.append(i)
+            else:
+                children[parent].append(i)
+        order = []
+        queue = list(roots) # this is to store the processing order
+        while queue:
+            i = queue.pop(0)
+            order.append(i)
+            queue.extend(children[i]) #this part is to deal with roots first, then add the children of the roots, then deal with the childeren and then grandchildren
+            
+    for i in order:
+        joint = joints[i]
+        parent = joint.parent
+        R_local = local_rotations[i] # this is the rotation of this joint relative to its parent
+        t_local = np.array(joint.translation, dtype=np.float32) # tjis is the translation of the joint ralative to its parent
+        # judge if the joint is root or not (parent=-1 is root)
+        if parent is None or parent <0:
+            world_rotations[i] =R_local
+            world_positions[i] = t_local + root_offset
+        else:
+            # for a non-root joint,the formula isR_world(i) = R_world(p(i)) R_local(i)
+            #p_world(i) = p_world(p(i)) + R_world(p(i)) t_local(i)
+            world_rotations[i] = world_rotations[parent] @ R_local
+            world_positions[i] = world_positions[parent] + world_rotations[parent] @ t_local
+
+
     return world_rotations, world_positions
